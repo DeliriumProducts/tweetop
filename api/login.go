da7a546ce23e526d2c89712543d7f659d4253698
Simo Aleksandrov <simo3003@me.com>
2019-08-12 17:32:57 +0200

    api

---
+++ api/login.go
@@ -0,0 +1,8 @@
+package handler
+
+import "net/http"
+
+// Handler is the main handler for logging in using the twitter API
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
